bb930d5f394b1c32df243b2395db73cc190d0694
blackpintz <rosewanjohi24@gmail.com>
2021-01-13 13:04:18 +0300

    did snapshot testing for the App component.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
-import propTypes from 'prop-types';
 import './App.css';
 import TvShowList from './components/TvShowList';
 import TvShowDetails from './components/TvShowDetails';
 
-const App = ({ tvShows }) => {
-  console.log(tvShows);
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={TvShowList} />
-        <Route exact path="/show/:id" render={routeProps => <TvShowDetails id={routeProps.match.params.id} routeProps={routeProps} />} />
-      </Switch>
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <Switch>
+      <Route exact path="/" component={TvShowList} />
+      <Route exact path="/show/:id" render={routeProps => <TvShowDetails id={routeProps.match.params.id} routeProps={routeProps} />} />
+    </Switch>
+  </div>
+);
 
-const mapStateToProps = ({ tvShows }) => ({
-  tvShows,
-});
-
-App.propTypes = {
-  tvShows: propTypes.arrayOf(propTypes.object),
-};
-
-App.defaultProps = {
-  tvShows: 'no value',
-};
-
-export default connect(mapStateToProps)(App);
+export default App;
--- src/tests/components/App.test.js
+++ src/tests/components/App.test.js
@@ -1,3 +1,9 @@
-test('renders learn react link', () => {
+import React from 'react';
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import App from '../../App';
 
+test('renders App correctly', () => {
+  const renderer = new ReactShallowRenderer();
+  renderer.render(<App />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
