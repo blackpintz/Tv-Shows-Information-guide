a113b263b498e5ecce971137c526bfc4ffb82f90
blackpintz <rosewanjohi24@gmail.com>
2021-01-15 18:31:28 +0300

    updated tests.

--- src/tests/components/TvShowDetails.test.js
+++ src/tests/components/TvShowDetails.test.js
@@ -16,15 +16,3 @@ test('should render TvShowList component with the list of tvShows', () => {
   const wrap = shallow(<TvShowDetails onFetch={onFetch} tvShow={tvShows[0]} routeProps={rProps} />);
   expect(wrap).toMatchSnapshot();
 });
-
-test('should render homepage when button is click', () => {
-  const onFetch = jest.fn();
-  const rProps = {
-    history: {
-      goBack: jest.fn(),
-    },
-  };
-  const wrap = shallow(<TvShowDetails onFetch={onFetch} tvShow={tvShows[0]} routeProps={rProps} />);
-  wrap.find('Button').simulate('click');
-  expect(rProps.history.goBack).toHaveBeenCalled();
-});
